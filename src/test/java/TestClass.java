bd574d575a74c1e51e0c49e2976549820dee4e21
Tobias Riemenschneider <Tobias.Riemenschneider@softwareag.com>
2022-11-17 10:14:23 +0100

    Added support for Linux. (#1)
    On Linux, added --add-host=host.docker.internal:host-gateway to Docker command used for running Nginx.
    Added additional logging.
    Removed unused dependencies since the issue can be reproduced using Vert.x WebClient, i.e., Apache HttpComponents HttpClient is not required for reproducing the issue.

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -12,7 +12,10 @@ import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import io.netty.util.internal.PlatformDependent;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
@@ -36,13 +39,16 @@ import io.vertx.ext.web.handler.LoggerHandler;
 @RunWith(VertxUnitRunner.class)
 public class TestClass {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestClass.class);
+	
 	private static final int HTTP_SERVER_PORT = Integer.getInteger("HTTP_SERVER_PORT", 9090);
 	private static final int LOADBALANCER_PORT = Integer.getInteger("LOADBALANCER_PORT", 9091);
 	private static final boolean LOG_ACTIVITY = Boolean.getBoolean("LOG_ACTIVITY");
 	
-	public static Future<HttpServer> startVertxHttpServer(Vertx vertx, int port, boolean logActivity) {
+	private static Future<HttpServer> startVertxHttpServer(Vertx vertx, int port, boolean logActivity) {
+		LOGGER.info("Starting Vert.x HttpServer.");
 		HttpServer httpServer = vertx.createHttpServer(new HttpServerOptions()
-				.setHost("localhost")
+				.setHost("0.0.0.0")
 				.setPort(port)
 				.setLogActivity(logActivity));
 		Router router = Router.router(vertx);
@@ -53,11 +59,14 @@ public class TestClass {
 				.handler(routingContext -> routingContext.response()
 						.end());
 		return httpServer.requestHandler(router)
-				.listen();
+				.listen()
+				.onSuccess(ignore -> LOGGER.info("Vert.x HttpServer successfully started."))
+				.onFailure(cause -> LOGGER.error("Failed to start Vert.x HttpServer.", cause));
 	}
 	
-	public static Future<String> startNginxLoadbalancer(Vertx vertx, int loadbalancerPort, int serverPort) {
-		return vertx.executeBlocking(promise -> {
+	private static Future<String> startNginxLoadbalancer(Vertx vertx, int loadbalancerPort, int serverPort) {
+		return vertx.<String>executeBlocking(promise -> {
+			LOGGER.info("Starting Nginx loadbalancer.");
 			try {
 				Path nginxConfTempFilePath = File.createTempFile("nginx", ".conf").toPath();
 				List<String> nginxConf = Arrays.asList(
@@ -65,7 +74,7 @@ public class TestClass {
 					"",
 					"http {",
 					"  server {",
-					"    listen " + loadbalancerPort + ";",
+					"    listen 0.0.0.0:" + loadbalancerPort + ";",
 					"    ",
 					"    location / {",
 					"      proxy_pass http://backend;",
@@ -75,53 +84,111 @@ public class TestClass {
 					"  upstream backend {",
 					"    server host.docker.internal:" + serverPort + ";",
 					"  }",
+					"  ",
 					"  client_max_body_size 10M;",
 					"}"
 				);
+				LOGGER.info("Using nginx.conf:\n{}", String.join("\n", nginxConf));
 				Files.write(nginxConfTempFilePath, nginxConf, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-				File outputTempFile = File.createTempFile("docker", null);
+				File outputTempFile = File.createTempFile("output", null);
+				File errorTempFile = File.createTempFile("error", null);
+				String[] command = PlatformDependent.isWindows() || PlatformDependent.isOsx()
+						? new String[] { "docker", "run", "--rm", "--detach", "--publish", loadbalancerPort + ":" + loadbalancerPort, "--volume", nginxConfTempFilePath + ":/etc/nginx/nginx.conf:ro", "nginx" }
+						: new String[] { "docker", "run", "--rm", "--detach", "--add-host", "host.docker.internal:host-gateway", "--publish", loadbalancerPort + ":" + loadbalancerPort, "--volume", nginxConfTempFilePath + ":/etc/nginx/nginx.conf:ro", "nginx" };
+				LOGGER.info("Using command '{}'", String.join(" ", command));
 				Process process = new ProcessBuilder()
-						.command("docker", "run", "--rm", "--detach", "--publish", loadbalancerPort + ":" + loadbalancerPort, "--volume", nginxConfTempFilePath + ":/etc/nginx/nginx.conf:ro", "nginx")
+						.command(command)
 						.redirectOutput(outputTempFile)
-						.redirectError(Redirect.INHERIT)
+						.redirectError(errorTempFile)
 						.start();
-				if (process.waitFor() == 0)
-					promise.complete(Files.readAllLines(outputTempFile.toPath()).get(0));
-				else
-					promise.fail("Command 'docker run' failed (" + process.exitValue() +").");
+				if (process.waitFor() == 0) {
+					LOGGER.info("Successfully started Nginx loadbalancer.");
+					promise.complete(Files.readString(outputTempFile.toPath(), StandardCharsets.UTF_8).strip());
+				}
+				else {
+					LOGGER.error("Failed to start Nginx loadbalancer. Process returned with exit code {}.", process.exitValue());
+					LOGGER.error(Files.readString(errorTempFile.toPath(), StandardCharsets.UTF_8).strip());
+					promise.fail(process.info().toString());
+				}
 			}
-			catch (Throwable t) {
-				promise.fail(t);
+			catch (Throwable cause) {
+				LOGGER.error("Failed to start Nginx loadbalancer.", cause);
+				promise.fail(cause);
 			}
 		});
 	}
 	
-	public static Future<Void> stopNginxLoadbalancer(Vertx vertx, String containerId) {
+	private static Future<String> waitForInitializationOfNginxLoadbalancer(Vertx vertx, String containerId) {
 		return vertx.executeBlocking(promise -> {
+			LOGGER.info("Waiting for initialization of Nginx loadbalancer.");
 			try {
-				System.out.println("### Logs from Nginx loadbalancer ###");
+				File outputTempFile = File.createTempFile("output", null);
+				File errorTempFile = File.createTempFile("error", null);
+				for (Future<String> future = promise.future(); !future.isComplete(); ) {
+					Process process = new ProcessBuilder()
+							.command("docker", "logs", containerId)
+							.redirectOutput(outputTempFile)
+							.redirectError(errorTempFile)
+							.start();
+					if (process.waitFor() == 0) {
+						if (Files.readString(outputTempFile.toPath(), StandardCharsets.UTF_8).contains("Configuration complete; ready for start up")) {
+							LOGGER.info("Nginx loadbalancer is up and running.");
+							promise.complete(containerId);
+						}
+					}
+					else {
+						LOGGER.error("Failed to wait for initialization of Nginx loadbalancer. Process returned with exit code {}.", process.exitValue());
+						LOGGER.error(Files.readString(errorTempFile.toPath(), StandardCharsets.UTF_8).strip());
+						promise.fail(process.info().toString());
+					}
+				}
+			}
+			catch (Throwable cause) {
+				LOGGER.error("Failed to wait for initialization of Nginx loadbalancer.", cause);
+				promise.fail(cause);
+			}
+		});
+	}
+	
+	private static Future<Void> stopNginxLoadbalancer(Vertx vertx, String containerId) {
+		return vertx.executeBlocking(promise -> {
+			LOGGER.info("Stopping Nginx loadbalancer.");
+			try {
+				File outputTempFile = File.createTempFile("output", null);
 				new ProcessBuilder()
 						.command("docker", "logs", containerId)
-						.inheritIO()
+						.redirectOutput(outputTempFile)
+						.redirectError(Redirect.DISCARD)
 						.start()
 						.waitFor();
-				System.out.println("### Logs from Nginx loadbalancer ###");
+				String logs = Files.readString(outputTempFile.toPath(), StandardCharsets.UTF_8);
+				if (!logs.isBlank())
+					LOGGER.info("Logs from Nginx loadbalancer:\n{}", logs);
+				outputTempFile = File.createTempFile("output", null);
+				File errorTempFile = File.createTempFile("error", null);
 				Process process = new ProcessBuilder()
 						.command("docker", "stop", containerId)
-						.inheritIO()
+						.redirectOutput(outputTempFile)
+						.redirectError(errorTempFile)
 						.start();
-				if (process.waitFor() == 0)
+				if (process.waitFor() == 0) {
+					LOGGER.info("Nginx loadbalancer successfully stopped.");
 					promise.complete();
-				else
-					promise.fail("Command 'docker stop' failed (" + process.exitValue() +").");
+				}
+				else {
+					LOGGER.error("Failed to stop Nginx loadbalancer. Process returned with exit code {}.", process.exitValue());
+					LOGGER.error(Files.readString(errorTempFile.toPath(), StandardCharsets.UTF_8).strip());
+					promise.fail(process.info().toString());
+				}
 			}
-			catch (Throwable t) {
-				promise.fail(t);
+			catch (Throwable cause) {
+				LOGGER.error("Failed to stop Nginx loadbalancer.", cause);
+				promise.fail(cause);
 			}
 		});
 	}
 	
-	public static class StatusCodes {
+	private static class StatusCodes {
 		
 		private List<Integer> list;
 		
@@ -153,7 +220,8 @@ public class TestClass {
 		
 	}
 	
-	public static Future<StatusCodes> runWebClient(Vertx vertx, int port, boolean logActivity, TestContext context) {
+	private static Future<StatusCodes> runWebClient(Vertx vertx, int port, boolean logActivity, TestContext context) {
+		LOGGER.info("Running Vert.x WebCient.");
 		HttpClient httpClient = vertx.createHttpClient(new HttpClientOptions()
 				.setLogActivity(logActivity));
 		WebClient webClient = WebClient.wrap(httpClient, new WebClientOptions()
@@ -197,12 +265,12 @@ public class TestClass {
 	public RepeatRule repeatRule = new RepeatRule();
 	
 	@Test
-	@Repeat(10)
+	@Repeat(1000)
 	public void test(TestContext context) {
 		Vertx vertx = runTestOnContext.vertx();
 		Future<HttpServer> httpServerFuture = startVertxHttpServer(vertx, HTTP_SERVER_PORT, LOG_ACTIVITY);
 		Future<String> loadbalancerFuture = startNginxLoadbalancer(vertx, LOADBALANCER_PORT, HTTP_SERVER_PORT);
-		CompositeFuture.all(httpServerFuture, loadbalancerFuture)
+		CompositeFuture.all(httpServerFuture, loadbalancerFuture.compose(containerId -> waitForInitializationOfNginxLoadbalancer(vertx, containerId)))
 				.compose(futures -> runWebClient(vertx, LOADBALANCER_PORT, LOG_ACTIVITY, context))
 				.eventually(ignore -> CompositeFuture.join(httpServerFuture.compose(HttpServer::close), loadbalancerFuture.compose(containerId -> stopNginxLoadbalancer(vertx, containerId))))
 				.onComplete(context.asyncAssertSuccess(statusCodes -> context.assertEquals(new StatusCodes(200, 200, 200, 413, 200, 200, 200), statusCodes)));
